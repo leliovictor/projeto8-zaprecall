546135258826f6a783bb25ab632e8633b51d9263
leliovictor <leliov.vieira@gmail.com>
2022-05-14 15:50:52 -0300

    Screen 02 layout done

---
+++ src/components/Question.js
@@ -0,0 +1,10 @@
+export default function Question() {
+  return (
+    <div className="content">
+      <div className="question">
+        <p>Pergunta 1</p>
+        <ion-icon name="play-outline"></ion-icon>
+      </div>
+    </div>
+  );
+}
--- src/components/ZapScreen.js
+++ src/components/ZapScreen.js
@@ -1,5 +1,18 @@
+import Logo from "../assets/images/logo.png";
+import Question from "./Question";
+
 export default function ZapScreen() {
-    return(
-        <h1>teste</h1>
-    );
+  return (
+    <section className="main-screen">
+      <header>
+        <img src={Logo} alt="logo" />
+        <h1>ZapRecall</h1>
+      </header>
+      <Question />
+      <Question />
+      <footer>
+        <p>0/4 CONCLUÍDOS</p>
+      </footer>
+    </section>
+  );
 }
