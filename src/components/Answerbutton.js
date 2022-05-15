55d3042e271f5771c7971d780bf7ff09dad82891
leliovictor <leliov.vieira@gmail.com>
2022-05-15 17:28:06 -0300

    Added dinamic number of missing questions

--- src/components/Answerbutton.js
+++ src/components/Answerbutton.js
@@ -1,6 +1,5 @@
 export default function AnswerButton ({text, color,setQuestion, data, setData}) {
-    console.log(data);
-    
+
     return (
     <div className={`answerButton back${color}`} onClick={()=>[setQuestion(color),setData([...data,color])]}>{text}</div>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Footer({numberQuestion, data}) {
   
-  const icons = {red:<ion-icon key='0' class="red" name="close-circle"></ion-icon>,yellow:<ion-icon key='1' class="yellow" name="help-circle"></ion-icon>, green: <ion-icon key='2' class='green' name="checkmark-circle"></ion-icon>};
+  const icons = {red:<ion-icon key="0" class="red" name="close-circle"></ion-icon>,yellow:<ion-icon key='1' class="yellow" name="help-circle"></ion-icon>, green: <ion-icon key='2' class='green' name="checkmark-circle"></ion-icon>};
   
   return (
     <footer>
       <div className="pontuation">
-        <p>0/{numberQuestion} CONCLUÍDOS</p>
+        <p>{data.length}/{numberQuestion} CONCLUÍDOS</p>
         <div className="answerIcons">
           {
             data.map((color)=>icons[color])
